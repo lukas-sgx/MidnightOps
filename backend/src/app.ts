e11f57bd1505ae401bff5e3f3ce17dacb8baca5e
lukas-sgx <lukas.soigneux@epitech.eu>
2025-12-04 23:11:08 +0100

    [+] Add backend config & basic routes

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import healthRoutes from './routes/health.routes';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use('/health', healthRoutes);
+
+export default app;

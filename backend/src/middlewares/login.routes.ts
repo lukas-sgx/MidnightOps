a9eb1bebcf6b00462055a0cb2be85afe5a368f35
lukas-sgx <lukas.soigneux@epitech.eu>
2025-12-20 23:51:01 +0100

    [+] Add mailauth & alpine docker image

--- backend/src/middlewares/login.routes.ts
+++ backend/src/middlewares/login.routes.ts
@@ -1,9 +1,36 @@
 import { pool } from '../config/db';
 import { Router } from 'express';
 import { generateToken } from './jwt';
+import nodemailer from 'nodemailer';
+import redisClient from '../config/redis';
 
-const JWT_SECRET = process.env.JWT_SECRET || "change_me";
 const router = Router();
+
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function send_code(dest: string, code: number) {
+  const transporter = nodemailer.createTransport({
+    host: "mail.soigneux.works",
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USER!,
+      pass: process.env.SMTP_PASS!,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Support" <noreply@soigneux.works>',
+    to: dest,
+    subject: "Your verification code",
+    html: "<p>Your verification code is: <b>" + code + "</b></p>",
+  });
+
+  console.log("Message ID:", info.messageId);
+}
 
 router.post('/login', async (req, res) => {
     const { email } = req.body;
@@ -14,9 +41,12 @@ router.post('/login', async (req, res) => {
             [email]
         );
 
-        if (result.rows.length === 0) {
-            return res.status(401).json({ message: 'Invalid credentials' });
+        if (result.rows.length === 1) {
+            const code = getRandomInt(100000, 999999);
+            await send_code(email, code);
+            await redisClient.setEx(`login_code_${email}`, 600, code.toString());
         }
+        return res.status(200).json({ message: 'If the email exists, a verification code has been sent.' });
         const token = generateToken({ userId: result.rows[0].id, email: result.rows[0].email });
         return res.status(200).json({ message: 'Login successful', user: result.rows[0], token: token});
     } catch (err) {
